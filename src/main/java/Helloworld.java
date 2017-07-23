072e394389cce9636657bd6a015d59b5858c0ac9
Zshllj20160221 <596986447@qq.com>
2017-07-23 13:23:53 +0800

    shiro 入门

---
+++ src/main/java/Helloworld.java
@@ -0,0 +1,18 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by zsh on 2017/7/23.
+ */
+public class Helloworld {
+
+     static final Logger logger = LoggerFactory.getLogger(Helloworld.class);
+
+     public static void main(String [] args){
+
+         logger.debug("**********");
+
+     }
+
+
+}
